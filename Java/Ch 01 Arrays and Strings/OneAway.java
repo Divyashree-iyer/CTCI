622dcfa1b48514c68b99a878d52a389d61451e6a
Divyashree Iyer <diyer171100@gmail.com>
2023-08-16 21:29:30 -0700

    ch1 q5 same func for insert and replace

--- Java/Ch 01 Arrays and Strings/OneAway.java
+++ Java/Ch 01 Arrays and Strings/OneAway.java
@@ -2,6 +2,35 @@
 public class OneAway
 {
 	
+	static boolean replaceAndInsertCheck(String[] strs)
+	{
+		if(strs[0].length() - strs[1].length() >1)
+			return false;
+		String s1 = strs[0].length()>=strs[1].length()?strs[1]:strs[0];
+		String s2 = strs[0].length()<strs[1].length()?strs[1]:strs[0];
+		
+		int index1=0, index2=0;
+		boolean flag = false;
+		
+		while(index1<s1.length() && index2<s2.length())
+		{
+			if(s1.charAt(index1)!=s2.charAt(index2))
+			{
+				if(flag)
+					return false;
+				flag = true;
+				if(s1.length()==s2.length())
+					index1++;
+			}
+			else
+			{
+				index1++;
+			}
+			index2++;
+		}
+		return true;
+	}
+	
 	static boolean replacementCheck(String[] strs)
 	{
 		int flag = 0;
@@ -45,12 +74,11 @@ public class OneAway
 	}
 
 	public static void main(String[] args)
-	{
-		
-		String[][] testStrings = {{"pale","bale"},{"pale","pale"}, {"pale","pal"},{"pal","pale"},{"pale","bade"},{"pales","pasle"}, {"pale",""},{"orange","pale"}};
+	{		
+		String[][] testStrings = {{"pal","pl"},{"pal","pele"},{"pale","bale"},{"pale","pale"}, {"pale","pal"},{"pal","pale"},{"pale","bade"},{"pales","pasle"}, {"pale",""},{"orange","pale"}};
 		for(String[] strs:testStrings)
 		{
-			System.out.println(strs[0] + ", " + strs[1] + " : " + allTestPassed(strs));
+			System.out.println(strs[0] + ", " + strs[1] + " : " + allTestPassed(strs) + " : " + replaceAndInsertCheck(strs));
 		}
 	}
 }
